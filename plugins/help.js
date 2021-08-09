ab17dd3dba38e88f5124c08d175a720a26643bd8
Vishnu Prasad <87913417+vishnu-memanakizhakathil@users.noreply.github.com>
2021-08-09 23:42:09 +0530

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'help', fromMe: false, desc: Lang.WP}, (async (messag
     
     
    
-  r_text[0] = "https://i.imgur.com/MZDCN2M.jpeg";
+  r_text[0] = "https://avatars.githubusercontent.com/u/87913417?v=4";
     
     
     var i = Math.floor(1*Math.random())
@@ -20,7 +20,7 @@ Asena.addCommand({pattern: 'help', fromMe: false, desc: Lang.WP}, (async (messag
 
     await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Meenakshi Message❤*
 
-*Hi I am Meenakshi,a whatsapp chat bot! Since i'm a chat bot,don't try to call me WhatsApp or send me spam messages! Or if you get noticed by the Bot admin you will be prevented from using this whatsapp Bot*
+*Hi I am Meenakshi,a whatsapp chat bot! Since i'm a chat bot,don't try to call me WhatsApp or send me spam messages! Or if you get noticed by the Bot admin you will be prevented from using this whatsapp Bot.Sending your photo and video to make stickers is at your own risk! We are not responsible for any misuse of this bot*
 
 `}) 
 
