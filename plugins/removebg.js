c7792fc7b1a2b87fa9f6b6cd37533113056c2df7
MUHAMMED JASIM <87895173+Jasimjz@users.noreply.github.com>
2021-08-19 17:24:18 +0530

    Update removebg.js

--- plugins/removebg.js
+++ plugins/removebg.js
@@ -52,7 +52,7 @@ if (Config.WORKTYPE == 'private') {
 		    fs.createWriteStream('rbg.png')
         );
     
-        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'Meenakshi.png', mimetype: Mimetype.png});
+        await message.client.sendMessage(message.jid,fs.readFileSync('rbg.png'), MessageType.document, {filename: 'AlexaJz.png', mimetype: Mimetype.png});
         await load.delete();
     }));
 }
