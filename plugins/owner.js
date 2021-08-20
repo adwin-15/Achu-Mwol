384554742dd0fc4298c313fca0694e5df65f75d1
Vishnu Prasad <87913417+vishnu-memanakizhakathil@users.noreply.github.com>
2021-08-21 00:56:10 +0530

    Create owner.js

---
+++ plugins/owner.js
@@ -0,0 +1,26 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://www.pixelstalk.net/wp-content/uploads/2016/03/Pikachu-wallpaper-HD-dowload.jpg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*MADE BY MUHAMMAD JASIM**
+*owner number wa.me/918888888888*
+*TYPE YOUR OWN*
+`}) 
+
+}));
