b784bf9a64bbab3117c5571e85e801829904465b
Vishnu Prasad <87913417+vishnu-memanakizhakathil@users.noreply.github.com>
2021-09-07 09:17:03 +0530

    Update up_down.js

--- plugins/up_down.js
+++ plugins/up_down.js
@@ -1,20 +1,36 @@
-/* Copyright (C) 2020 Yusuf Usta.
+const { MessageType, Mimetype } = require("@adiwajshing/baileys");
+const Asena = require("../Utilis/events");
+// const config = require('../config');
+const moment = require("moment");
+const {
+  getName,
+  getBuffer,
+  getJson,
+  IdentifySong,
+} = require("../Utilis/download");
+const { emoji } = require("../Utilis/Misc");
+const { audioCut } = require("../Utilis/fFmpeg");
 
-Licensed under the  GPL-3.0 License;
-you may not use this file except in compliance with the License.
-
-WhatsAsena - Yusuf Usta
-
-
-const Asena = require('../events');
-const {MessageType, Mimetype} = require('@adiwajshing/baileys');
-const got = require('got')
-const fs = require('fs')
-const Language = require('../language');
-const Lang = Language.getString('up_down');
-
-Asena.addCommand({pattern: 'download ?(.*)', fromMe: true, desc: Lang.MEMES_DESC, usage: 'meme top;bottom'}, (async (message, match) => {    
-    if (message.reply_message === false) return await message.sendMessage(Lang.NEED_REPLY);
-
-}));
-*/
+Asena.addCommand(
+  { pattern: "find", fromMe: true, desc: "Identify song." },
+  async (message, match) => {
+    if (
+      !message.reply_message ||
+      (!message.reply_message.audio && !message.reply_message.video)
+    )
+      return await message.sendMessage("*Reply to a audio.*");
+    let location = await message.reply_message.downloadAndSaveMediaMessage(
+      "find"
+    );
+    let buff = await audioCut(location, 0, 15, "findo");
+    const data = await IdentifySong(buff);
+    if (!data) return;
+    if (!data.status) return await message.sendMessage("*Not Found*");
+    let result =
+      "```" +
+      `Title    : ${data.data.title}\nArtists  : ${data.data.artists}
+Genre    : ${data.data.genre}\nAlbum    : ${data.data.album}\nReleased : ${data.data.release_date}` +
+      "```";
+    return await message.sendMessage(result, { quoted: message.quoted });
+  }
+);
