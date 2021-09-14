67fe10af1adaa4483bbc41d8d9aebd02e2b360f8
adwin-15 <90175633+adwin-15@users.noreply.github.com>
2021-09-14 20:00:37 +0530

    Update fakeid.js

--- plugins/fakeid.js
+++ plugins/fakeid.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 vishnu-kayamkulam
+/* Copyright (C) 2021 adwin-15
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-MEENKASHI*/
+Achu-Mwol*/
 
 
 const Asena = require('../events');
