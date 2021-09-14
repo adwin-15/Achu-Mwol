da00c1cb87ff65fda32c7150a16a7bf89e8f0c19
adwin-15 <90175633+adwin-15@users.noreply.github.com>
2021-09-14 15:33:30 +0530

    Update _asena.js

--- plugins/_asena.js
+++ plugins/_asena.js
@@ -1,4 +1,4 @@
-/* Copyright (C) 2021 edwin shaju
+/* Copyright (C) 2021 edwin-shaju
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
 WhatsAsena - Yusuf Usta
