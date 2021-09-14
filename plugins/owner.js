018e4c0d0f4f10e51ba0adc938da3b7b7bdc342c
adwin-15 <90175633+adwin-15@users.noreply.github.com>
2021-09-14 19:57:02 +0530

    Update owner.js

--- plugins/owner.js
+++ plugins/owner.js
@@ -11,14 +11,13 @@ Asena.addCommand({pattern: 'owner', fromMe: false, desc: Lang.WP}, (async (messa
     
     
    
-  r_text[0] = "anima-08-chai8.jpg";
     
     
     var i = Math.floor(1*Math.random())
 
     var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
 
-    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Vishnu Mkz*
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*Edwin Shaju*
 *owner number wa.me/919072719401*
 *instagram  : https://www.instagram.com/adwin_15?r=nametag*
 *githublink       https://github.com/adwin-15/Achu-Mwol*
