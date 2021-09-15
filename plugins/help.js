72f0f94e14ab858c83db5c43df47e09f479bcda6
adwin-15 <90175633+adwin-15@users.noreply.github.com>
2021-09-15 19:39:22 +0530

    Update help.js

--- plugins/help.js
+++ plugins/help.js
@@ -11,7 +11,7 @@ Asena.addCommand({pattern: 'help', fromMe: false, desc: Lang.WP}, (async (messag
     
     
    
-  r_text[0] = "anima-08-chai8.jpg";
+  r_text[0] = "https://raw.githubusercontent.com/adwin-15/Achu-Mwol/master/anima-08-chai8.jpg";
     
     
     var i = Math.floor(1*Math.random())
